a74c7f5270f303c21c1dc7761326ee09b4971baf
TheDancerCodes <rojtaracha@gmail.com>
2018-03-20 10:27:23 +0300

    [FEATURE] Activity/Layout Relationship:
    [+] Add a variable to reference to the spinner in the Activity.
    [+] Delete the FAB.

--- app/src/main/java/com/thedancercodes/notekeep3r/NoteActivity.java
+++ app/src/main/java/com/thedancercodes/notekeep3r/NoteActivity.java
@@ -1,13 +1,11 @@
 package com.thedancercodes.notekeep3r;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Spinner;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -15,17 +13,12 @@ public class NoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Spinner Variable
+        Spinner spinnerCourses = (Spinner) findViewById(R.id.spinner_courses);
+
     }
 
     @Override
